c455cd31023d8cf865906bea86628be6064fad85
joonas.fi <pompo500@xs.fi>
2017-01-16 14:52:30 +0200

    Update make-zip-archive.sh to archive both components and stamp them by the date

--- bin/make-zip-archive.sh
+++ bin/make-zip-archive.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -eu
 
-rm -f canary.zip && cd src/ && zip ../canary.zip index.js utils.js
+date_formatted=`date +%Y-%m-%d`
+
+filename_canary="alertmanager-canary-$date_formatted.zip"
+filename_alertmanager="alertmanager-$date_formatted.zip"
+
+(rm -f "$filename_canary" && cd alertmanager-canary/ && zip "../$filename_canary" index.js utils.js)
+
+(rm -f "$filename_alertmanager" && cd alertmanager && zip "../$filename_alertmanager" index.js)
