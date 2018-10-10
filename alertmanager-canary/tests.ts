8b71d7509abbb5b11d5f12d45d3c498b35cf4a4f
Joonas Loppi <joonas@joonas.fi>
2018-10-10 17:22:32 +0300

    fix typo

--- alertmanager-canary/tests.ts
+++ alertmanager-canary/tests.ts
@@ -37,7 +37,7 @@ class TestMockActions implements ActionInterface {
 			case 'https://this-one-succeeds.com/':
 			case 'https://this-one-fails.com/':
 				return Promise.resolve(
-					'they body you need, but not the body you deserve',
+					'the body you need, but not the body you deserve',
 				);
 			case 'https://this-one-always-timeouts.net/':
 				return Promise.reject(new Error('Faking timeout'));
@@ -83,7 +83,7 @@ handlerInternal(testMockActions)
 		assertEqual(logs[0], '✓  https://this-one-succeeds.com/ @ 0ms => OK');
 		assertEqual(
 			logs[1],
-			'✗  https://this-one-fails.com/ @ 0ms => find<will not be found> NOT in body<they body you need, but not the body you deserve>',
+			'✗  https://this-one-fails.com/ @ 0ms => find<will not be found> NOT in body<the body you need, but not the body you deserve>',
 		);
 		assertEqual(
 			logs[2],
@@ -98,7 +98,7 @@ handlerInternal(testMockActions)
 		assertEqual(logs[6], '   failed; re-trying once in 1000ms');
 		assertEqual(
 			logs[7],
-			'✗  https://this-one-fails.com/ @ 0ms => find<will not be found> NOT in body<they body you need, but not the body you deserve>',
+			'✗  https://this-one-fails.com/ @ 0ms => find<will not be found> NOT in body<the body you need, but not the body you deserve>',
 		);
 		assertEqual(
 			logs[8],
@@ -117,7 +117,7 @@ handlerInternal(testMockActions)
 		assertEqual(alerts[0].subject, 'https://this-one-fails.com/');
 		assertEqual(
 			alerts[0].details,
-			'find<will not be found> NOT in body<they body you need, but not the body you deserve>',
+			'find<will not be found> NOT in body<the body you need, but not the body you deserve>',
 		);
 
 		assertEqual(alerts[1].subject, 'https://this-one-always-timeouts.net/');
